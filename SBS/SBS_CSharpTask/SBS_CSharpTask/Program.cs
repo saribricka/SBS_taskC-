2012b7d2b08011157c1bad6b76896c9ba4fa436d
saribricka <sara.b@myappfree.com>
2022-01-24 17:11:35 +0100

    first commit

---
+++ SBS/SBS_CSharpTask/SBS_CSharpTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SBS_CSharpTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
